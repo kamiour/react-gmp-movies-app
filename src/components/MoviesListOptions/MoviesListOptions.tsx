40a17367b0bdc474182e0206e92dc69480171508
Mykhailo Kazakov <kamiournew@gmail.com>
2022-05-19 15:04:44 +0300

    Cleaned project

--- src/components/MoviesListOptions/MoviesListOptions.tsx
+++ src/components/MoviesListOptions/MoviesListOptions.tsx
@@ -1,5 +1,4 @@
 import { PropsWithChildren } from 'react';
-import './MoviesListOptions.scss';
 
 function MoviesListOptions(props: PropsWithChildren<{}>) {
   return <div className="movies-list-options">{props.children}</div>;
