2ea08a0ecee4f4361bd4173d9ff05d651b249ed0
Mykhailo Kazakov <kamiournew@gmail.com>
2022-06-04 16:45:10 +0300

    Started implementing router

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
 import { store } from './store';
+import './index.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/">
+            <Redirect to="/search" />
+          </Route>
+          <Route path="/search" element={<App />} />
+          <Route path="/about" element={<>About</>} />
+          <Route path="*" element={<>Not found</>} />
+        </Routes>
+      </Router>
     </Provider>
   </React.StrictMode>
 );
