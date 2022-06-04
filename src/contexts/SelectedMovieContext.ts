39796be5d6cce558c3f1bbac84953b77a0d46411
Mykhailo Kazakov <kamiournew@gmail.com>
2022-06-04 16:48:13 +0300

    Removed contexts and unused useFetchMovies hook

--- src/contexts/SelectedMovieContext.ts
+++
@@ -1,9 +0,0 @@
-import React, { Dispatch, SetStateAction } from 'react';
-import { Movie } from '../models/Movie';
-
-export interface SelectedMovieState {
-  selectedMovie: Movie | null;
-  setSelectedMovie: Dispatch<SetStateAction<Movie | null>>;
-}
-
-export const SelectedMovieContext = React.createContext<SelectedMovieState>({ selectedMovie: null, setSelectedMovie: () => {} });
