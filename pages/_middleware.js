97f61d9dc521d021257e00f67f8e2f4c35e5b211
Mykhailo Kazakov <kamiournew@gmail.com>
2022-06-10 01:49:34 +0300

    Added middleware redirect

---
+++ pages/_middleware.js
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+
+export async function middleware(req) {
+  const { pathname } = req.nextUrl;
+  if (pathname == '/') {
+    const url = req.nextUrl.clone();
+    url.pathname = '/search';
+
+    return NextResponse.redirect(url);
+  }
+  return NextResponse.next();
+}
