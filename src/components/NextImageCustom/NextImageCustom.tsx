821c823339586352f124d9e996afeba5cf0cbe00
Mykhailo Kazakov <kamiournew@gmail.com>
2022-06-20 22:29:07 +0300

    Handled image unoptimized property via environment variable

--- src/components/NextImageCustom/NextImageCustom.tsx
+++ src/components/NextImageCustom/NextImageCustom.tsx
@@ -4,15 +4,19 @@ import Image from 'next/image';
 export const NextImageCustom = ({ alt, ...props }) => {
   const [src, setSrc] = useState(props.src);
 
+  const isUnoptimized = process.env.NODE_ENV === 'development';
+
   return (
-    <Image
-      unoptimized // to make it work in Storybook
-      {...props}
-      alt={alt} // to fix lint warning
-      src={src}
-      placeholder="blur"
-      blurDataURL="/images/img-placeholder.png"
-      onError={() => setSrc('/images/img-placeholder.png')}
-    />
+    <>
+      <Image
+        unoptimized={isUnoptimized} // to make it work in Storybook
+        {...props}
+        alt={alt} // to fix lint warning
+        src={src}
+        placeholder="blur"
+        blurDataURL="/images/img-placeholder.png"
+        onError={() => setSrc('/images/img-placeholder.png')}
+      />
+    </>
   );
 };
